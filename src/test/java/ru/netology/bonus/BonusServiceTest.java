fad1581213bd5f4d10086169cd987576a31375e3
Aqz <Kzak.Gluhov@gmail.com>
2022-04-07 21:24:36 +0300

    SQRService

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -1,16 +1,13 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import ru.netology.bonus.BonusService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
-    @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    void shouldCalculateForRegisteredAndUnderLimit(String name, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -18,7 +15,6 @@ class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-
     @Test
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
